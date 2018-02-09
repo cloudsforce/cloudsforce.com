364c71613f7e77d420e16235e6173966699c1fee
Wayland <wayland.zhang@mobingi.com>
2018-02-09 18:05:45 +0900

    Update mobingi-init.sh

--- mobingi-init.sh
+++ mobingi-init.sh
@@ -2,4 +2,5 @@
 apt-get update
 apt-get -y install curl php-cli git
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+cd /var/www/html
 composer install
