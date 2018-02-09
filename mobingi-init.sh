4233a2f45659cbd9e6fb8a389bffcdecaa9fc2c4
Wayland <wayland.zhang@mobingi.com>
2018-02-09 18:00:38 +0900

    Create mobingi-init.sh
    Auto code deployment through Mobingi-ALM.

---
+++ mobingi-init.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+apt-get update
+apt-get -y install curl php-cli git
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+composer install
